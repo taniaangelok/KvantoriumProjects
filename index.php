8538f91c165a1f1b2ad7ba188ac683f920e75e3e
taniaangelok <52499876+taniaangelok@users.noreply.github.com>
2019-07-03 17:28:48 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head><link rel="stylesheet" href="Untitled-1.css"></head>
+<h1> самые интересные проекты</h1>
+<div class ="f">
+<?php for ($i=0;$i<10;$i++){?>
+
+<div class="a"> 
+acv
+</div>
+
+<?php }?>
+</div>
