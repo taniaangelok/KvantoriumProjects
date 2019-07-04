184adedf51d7f00bc775b2f473518c1534ae0390
blufox21 <lunevm@list.ru>
2019-07-04 14:39:17 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <head>
 	<title>Главная</title>
     <link rel="stylesheet" href="styles/style.css">
-    <link rel="stylesheet" href="Untitled-1.css">
 	<link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,700|Ubuntu:700&display=swap&subset=cyrillic" rel="stylesheet">  
 	<meta charset="utf-8">
 </head>
@@ -31,40 +30,40 @@
 	</header>
 	<div class="main">
 		<div class="kvantum" id="Bio">
-			<h1>Самые популярные Био</h1>
-			<div class="proj">
-				<div class="rating">
-					<div class="likes">
-						<span>20 L</span>
-						<img src="res/images/arrow.png">
-					</div>
-
-					<div class="dislikes">
-						<span>10 DL</span>
-						<img src="res/images/arrowD.png">
-					</div>
-				</div>
+			<h1 class="kvantum-name">Самые популярные Био</h1>
+			<div class="featured">
+				<?php for ($i=0;$i<4;$i++){include("project-block.php");}?>
 			</div>
 		</div>
 		<div class="kvantum" id="It">
-            <h1>Самые популярные Айти</h1>
-            <?php for ($i=0;$i<10;$i++){?>
-                <div class="a"> 
-                acv
-                </div>
-            <?php }?>
+            <h1 class="kvantum-name">Самые популярные Айти</h1>
+            <div class="featured">
+				<?php for ($i=0;$i<4;$i++){include("project-block.php");}?>
+			</div>
 		</div>
 		<div class="kvantum" id="Robo">
-			<h1>Самые популярные Робо</h1>
+			<h1 class="kvantum-name">Самые популярные Робо</h1>
+			<div class="featured">
+				<?php for ($i=0;$i<4;$i++){include("project-block.php");}?>
+			</div>
 		</div>
 		<div class="kvantum" id="Energy">
-			<h1>Самые популярные Энерджи</h1>	
+			<h1 class="kvantum-name">Самые популярные Энерджи</h1>
+			<div class="featured">
+				<?php for ($i=0;$i<4;$i++){include("project-block.php");}?>
+			</div>	
 		</div>
 		<div class="kvantum" id="Design">
-			<h1>Самые популярные Промдизайн</h1>
+			<h1 class="kvantum-name">Самые популярные Промдизайн</h1>
+			<div class="featured">
+				<?php for ($i=0;$i<4;$i++){include("project-block.php");}?>
+			</div>
 		</div>
 		<div class="kvantum" id="Hitech">
-			<h1>Самые популярные Хай-тек</h1>
+			<h1 class="kvantum-name">Самые популярные Хай-тек</h1>
+			<div class="featured">
+				<?php for ($i=0;$i<4;$i++){include("project-block.php");}?>
+			</div>
 		</div>
 	</div>
 	<footer>
